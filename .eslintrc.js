00f8d84281a06e2df3cbbd089dcd5435535542bb
ydroulis <lombardi.droulis@gmail.com>
2024-01-12 09:30:02 -0300

    editing eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,23 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-native', 'prettier', 'react-hooks'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': 'off',
+    'react-native/no-color-literals': 'off',
+    'react-native/sort-styles': 'off',
+    'react-native/no-unused-styles': 'off',
+    'no-undef': 'off',
+  },
 };
